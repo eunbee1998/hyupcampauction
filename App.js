b414f4f145e8971fb52f164ec156f0ec4dea7734
eunbee1998 <eun061213@gmail.com>
2025-07-10 13:01:42 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>🎯 Hyupcamp 경매 프로그램 (React 버전)</h1>
+      <p>index.html 누락 오류를 해결했습니다. 이제 정상적으로 빌드될 거예요!</p>
+    </div>
+  );
+}
+
+export default App;
