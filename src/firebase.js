69634f9287d05bb02ab0b35249c0741f9b017103
eunbee1998 <eun061213@gmail.com>
2025-07-11 18:41:50 +0900

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -6,10 +5,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyDVB_Wx3yTmCkjX289K7p55zQe6JtXwqbM",
   authDomain: "hyupcamp-auction.firebaseapp.com",
   projectId: "hyupcamp-auction",
-  storageBucket: "hyupcamp-auction.firebasestorage.app",
+  storageBucket: "hyupcamp-auction.appspot.com",
   messagingSenderId: "715272776933",
   appId: "1:715272776933:web:fb7672b57be20ba520380c"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
--- src/pages/AuctionPage.js
+++ src/pages/AuctionPage.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { db } from "../firebase";
+import { collection, onSnapshot } from "firebase/firestore";
 
 function AuctionPage({ user, isAdmin }) {
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "auction/status"), (snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log("실시간 데이터:", doc.id, doc.data());
+      });
+    });
+    return () => unsub();
+  }, []);
+
   return (
     <div style={{ textAlign: "center", marginTop: "20vh" }}>
       <h1>Hyupcamp 자낳대 경매</h1>
       <h2>{user}님 ({isAdmin ? "관리자" : "팀장"}) 환영합니다!</h2>
-      <p>여기에 경매 시스템 UI가 구성됩니다.</p>
+      <p>여기에 실시간 동기화된 경매 UI가 표시됩니다.</p>
     </div>
   );
 }
