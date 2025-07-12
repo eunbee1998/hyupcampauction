a8a3507ce351c80351b48ab6b68794d3188ad5a0
eunbee1998 <eun061213@gmail.com>
2025-07-12 14:20:50 +0900

    Delete src directory

--- src/utils/auctionLogic.js
+++
@@ -1,6 +0,0 @@
-
-export function parseCardFileName(filename) {
-  const [num, name, positionWithExt] = filename.split("_");
-  const position = positionWithExt.replace('.png', '').toUpperCase();
-  return { number: num, name, position };
-}
