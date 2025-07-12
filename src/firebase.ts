36379d66e6df29afd312d1d7d57958d5f6e12682
eunbee1998 <eun061213@gmail.com>
2025-07-12 16:09:00 +0900

    Add files via upload

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVB_Wx3yTmCkjX289K7p55zQe6JtXwqbM",
+  authDomain: "hyupcamp-auction.firebaseapp.com",
+  projectId: "hyupcamp-auction",
+  storageBucket: "hyupcamp-auction.appspot.com",
+  messagingSenderId: "715272776933",
+  appId: "1:715272776933:web:fb7672b57be20ba520380c"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
